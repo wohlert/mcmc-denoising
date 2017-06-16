392e1a72270d87a1ba9dab4949330ef767c4630e
wohlert <wohlert.jesper@gmail.com>
2017-06-16 15:18:24 +0200

    edge detection

--- cpp/potts.cpp
+++ cpp/potts.cpp
@@ -5,39 +5,103 @@
 
 using namespace std;
 
+const size_t height = 400;
+const size_t width  = 328;
+
 float phi(float u) {
-  //return log(1 + pow(u, 2));
-  return u;
+  return log(1 + u*u);
 }
 
 template<size_t H, size_t W>
-unsigned int neighbourhood(const double (&X)[H][W],
-                           const unsigned int i, const unsigned int j,
-                           const unsigned int colour)
+void saveImage(const double (&Z)[H][W], const unsigned int k)
+{
+  string filename = "history/history_" + std::to_string(k) + ".txt";
+  ofstream saveFile(filename, ofstream::out);
+
+  if (saveFile.is_open()) {
+    for (size_t i = 0; i < H; i++) {
+      for (size_t j = 0; j < W; j++) {
+        saveFile << Z[i][j] << " ";
+      }
+    }
+  }
+
+  saveFile.close();
+}
+
+template<size_t H, size_t W>
+double neighbourhood(const double (&X)[H][W], const double colour,
+                     const int i, const int j)
 {
   double colour_sum = 0;
-  double w1 = 1;
-  double w2 = 0.5;
-  double w3 = 0.25;
-  double delta = 100;
+  double w1    = 1;
+  double w2    = 0.5;
+  double w3    = 0.25;
+  double delta = 50;
+  double diff;
 
   // Left, right, up, down
-  if (i-1 > -1)  { colour_sum += w1 * phi(X[i-1][j] - colour)/delta; }
-  if (i+1 <  H)  { colour_sum += w1 * phi(X[i+1][j] - colour)/delta; }
-  if (j+1 <  W)  { colour_sum += w1 * phi(X[i][j+1] - colour)/delta; }
-  if (j-1 > -1)  { colour_sum += w1 * phi(X[i][j-1] - colour)/delta; }
+  //if ((W/2 - i) > -1) {
+    //diff = (X[i-1][j] - colour)/delta;
 
+    if (X[i-1][j] != colour) {
+      diff = 1;
+    } else {
+      diff = 0;
+    }
+    colour_sum += diff;
+  //}
+
+  //if (i+W < H/2) {
+    //diff = (X[i+1][j] - colour)/delta;
+
+    if (X[i+1][j] != colour) {
+      diff = 1;
+    } else {
+      diff = 0;
+    }
+
+    colour_sum += diff;
+  //}
+
+  //if (j+1 < W/2) {
+    //diff = (X[i][j+1] - colour)/delta;
+
+    if (X[i][j+1] != colour) {
+      diff = 1;
+    } else {
+      diff = 0;
+    }
+
+    colour_sum += diff;
+  //}
+  //if ((H/2 - i) > -1) {
+    //diff = (X[i][j-1] - colour)/delta;
+
+    if (X[i][j-1] != colour) {
+      diff = 1;
+    } else {
+      diff = 0;
+    }
+
+    colour_sum += diff;
+  //}
+
+  colour_sum = w1 * colour_sum;
+
+  /*
   // Diagonal
-  if (i-1 > -1 && j-1 > -1)  { colour_sum += w2 * phi(X[i-1][j-1] - colour)/delta; }
-  if (i+1 <  H && j-1 > -1)  { colour_sum += w2 * phi(X[i+1][j-1] - colour)/delta; }
-  if (i+1 <  H && j+1 <  W)  { colour_sum += w2 * phi(X[i+1][j+1] - colour)/delta; }
-  if (i-1 > -1 && j+1 <  W)  { colour_sum += w2 * phi(X[i-1][j+1] - colour)/delta; }
+  if (i-1 > -1 && j-1 > -1)  { colour_sum += phi(X[i-1][j-1] - colour)/delta; }
+  if (i+1 <  H && j-1 > -1)  { colour_sum += phi(X[i+1][j-1] - colour)/delta; }
+  if (i+1 <  H && j+1 <  W)  { colour_sum += phi(X[i+1][j+1] - colour)/delta; }
+  if (i-1 > -1 && j+1 <  W)  { colour_sum += phi(X[i-1][j+1] - colour)/delta; }
 
   // Extremal
-  if (i-2 > -1) { colour_sum += w3 * phi(X[i-2][j] - colour)/delta; }
-  if (i+2 <  H) { colour_sum += w3 * phi(X[i+2][j] - colour)/delta; }
-  if (j+2 <  W) { colour_sum += w3 * phi(X[i][j+2] - colour)/delta; }
-  if (j-2 > -1) { colour_sum += w3 * phi(X[i][j-2] - colour)/delta; }
+  if (i-2 > -1) { colour_sum += phi(X[i-2][j] - colour)/delta; }
+  if (i+2 <  H) { colour_sum += phi(X[i+2][j] - colour)/delta; }
+  if (j+2 <  W) { colour_sum += phi(X[i][j+2] - colour)/delta; }
+  if (j-2 > -1) { colour_sum += phi(X[i][j-2] - colour)/delta; }
+  */
 
   // First derivative
   return colour_sum;
@@ -48,9 +112,11 @@ void metropolisHastings(double (&X)[H][W], const double (&Y)[H][W],
                         const unsigned int iterations,
                         const double noise, const double beta)
 {
-  unsigned int inverse, k;
+  unsigned int k;
+  double inverse;
   double h, hInverse;
-  double d, dInverse;
+  double d = 0;
+  double dInverse = 0;
   double p, u;
 
   // Use Mersenne Twister random number generator
@@ -59,6 +125,8 @@ void metropolisHastings(double (&X)[H][W], const double (&Y)[H][W],
   uniform_real_distribution<double> U(0, 1);
   normal_distribution<double>       N(0, noise);
 
+  double Z[height][width];
+
   for (k = 0; k < iterations; k++) {
     if (k % 5 == 0) {
       cout << "iteration: " << k << endl;
@@ -66,23 +134,25 @@ void metropolisHastings(double (&X)[H][W], const double (&Y)[H][W],
 
     for (size_t i = 0; i < H; i++) {
       for (size_t j = 0; j < W; j++) {
-        double normal = N(generator);
-        inverse  = (X[i][j] + normal);
+
+        inverse  = (X[i][j] + N(generator));
 
         h        = -1.0/(2*noise) * pow((Y[i][j] - X[i][j]), 2);
         hInverse = -1.0/(2*noise) * pow((Y[i][j] - inverse), 2);
 
-        d        = beta * neighbourhood(X, i, j, X[i][j]) + h;
-        dInverse = beta * neighbourhood(X, i, j, inverse) + hInverse;
+        d        = beta * neighbourhood(X, X[i][j], i, j) + h;
+        dInverse = beta * neighbourhood(X, inverse, i, j) + hInverse;
 
         u = U(generator);
         p = exp(fmin(0, dInverse - d));
 
         if (u < p) {
-          X[i][j] = neighbourhood(X, i, j, inverse);
+          X[i][j] = Y[i][j];
         }
+        Z[i][j] = neighbourhood(Y, Y[i][j], i, j);
       }
     }
+    saveImage(Z, k);
   }
   cout << "iteration: " << k << endl;
 }
@@ -97,25 +167,26 @@ int main(int argc, char const *argv[]) {
   if (argc > 3) beta  = atof(argv[3]);
 
   // Load in files
-  ifstream inputFile("data/noisy.txt", ifstream::in);
-  ofstream outputFile("denoised.txt", ofstream::out);
+  ifstream inputFile("data/original.txt", ifstream::in);
 
   // Instantiate Mersenne Twister
   random_device device;
   mt19937 generator(device());
   uniform_real_distribution<double> U(0, 1);
 
-  // Iniitalise X and Y
-  const size_t height = 400;
-  const size_t width  = 328;
-
+  // Inititalise X and Y
   double X[height][width];
   double Y[height][width];
+
+  for (size_t i = 0; i < height; i++) {
+    for (size_t j = 0; j < width; j++) {
+      X[i][j] = U(generator);
+    }
+  }
 
   if (inputFile.is_open()) {
     for (size_t i = 0; i < height; i++) {
       for (size_t j = 0; j < width; j++) {
-        X[i][j] = U(generator);
         inputFile >> Y[i][j];
       }
     }
@@ -124,6 +195,7 @@ int main(int argc, char const *argv[]) {
   // Perform computation
   metropolisHastings(X, Y, iterations, noise, beta);
 
+  ofstream outputFile("denoised.txt", ofstream::out);
   // Write output to file
   if (outputFile.is_open()) {
     for (size_t i = 0; i < height; i++) {
--- cpp/to_img.py
+++ cpp/to_img.py
@@ -3,3 +3,11 @@ from scipy.misc import imsave
 
 A = np.loadtxt("denoised.txt")
 imsave("denoised.jpg", A.reshape(-1, 400))
+
+import glob
+
+files = glob.glob("history/*.txt")
+
+for i, file in enumerate(files):
+    A = np.loadtxt(file)
+    imsave("history/history_{}.jpg".format(i), A.reshape(-1, 400))
